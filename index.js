d84c57c5b5456fb080d20b46b8eb37a696f3c605
Elmuratovich <makhmudkuzikaev@gmail.com>
2022-05-08 19:12:48 +0600

    GRUD taza node.js menen tuzuu

---
+++ index.js
@@ -0,0 +1,90 @@
+const http = require("http");
+const { v4 } = require("uuid");
+const getBodyData = require("./util");
+
+const books = [
+  {
+    id: 1,
+    title: "book n1",
+    pages: 250,
+    author: "Writer 1",
+  },
+];
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/books" && req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "application/json charset=utf8",
+    });
+
+    const resp = {
+      status: "OK",
+      books,
+    };
+
+    res.end(JSON.stringify(resp));
+  } else if (req.url === "/books" && req.method === "POST") {
+    const data = await getBodyData(req);
+    const { title, pages, author } = JSON.parse(data);
+    const newBook = {
+      id: v4(),
+      title,
+      pages,
+      author,
+    };
+
+    books.push(newBook);
+    const resp = {
+      status: "Created",
+      book: newBook,
+    };
+
+    res.writeHead(200, {
+      "Content-Type": "application/json charset=utf8",
+    });
+    res.end(JSON.stringify(resp));
+  } else if (req.url.match(/\/books\/\w+/) && req.method === "GET") {
+    const id = req.url.split("/")[2];
+    const book = books.find((b) => b.id === id);
+    res.writeHead(200, {
+      "Content-Type": "application/json charset=utf8",
+    });
+    const resp = {
+      status: "OK",
+      book,
+    };
+    res.end(JSON.stringify(resp));
+  } else if (req.url.match(/\/books\/\w+/) && req.method === "PUT") {
+    const id = req.url.split("/")[2];
+    const data = await getBodyData(req);
+    const { title, pages, author } = JSON.parse(data);
+    const idx = books.findIndex((b) => b.id === id);
+    const changedBook = {
+      id: v4(),
+      title: title,
+      pages: pages,
+      author: author,
+    };
+    books[idx] = changedBook;
+    res.writeHead(200, {
+      "Content-Type": "application/json charset=utf8",
+    });
+    const resp = {
+      status: "OK",
+      book: changedBook,
+    };
+    res.end(JSON.stringify(resp));
+  } else if (req.url.match(/\/books\/\w+/) && req.method === "DELETE") {
+    const id = req.url.split("/")[2];
+    const book = books.find((b) => b.id === id);
+    res.writeHead(200, {
+      "Content-Type": "application/json charset=utf8",
+    });
+    const resp = {
+      status: "DELETED",
+    }
+    res.end(JSON.stringify(resp))
+  }
+});
+
+server.listen(3000, () => console.log("Server running on port: 3000"));
